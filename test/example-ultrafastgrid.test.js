0c7806f72069ae4212d2f9f3acd89a39b5f1d99d
Tal Hayut <tal.hayut@applitools.com>
2020-12-09 21:42:47 +0200

    docs: updating tutorial with the new concurrency api

--- test/example-ultrafastgrid.test.js
+++ test/example-ultrafastgrid.test.js
@@ -2,6 +2,7 @@
 
 const {
     VisualGridRunner,
+    RunnerOptions,
     Eyes,
     Target,
     Configuration,
@@ -25,7 +26,8 @@ describe('playwright', function () {
         page = await context.newPage();
         
         // Create a runner with concurrency of 1
-        runner = new VisualGridRunner(1);
+        const runnerOptions = new RunnerOptions().testConcurrency(1)
+        runner = new VisualGridRunner(runnerOptions);
 
         // Create Eyes object with the runner, meaning it'll be a Visual Grid eyes.
         eyes = new Eyes(runner);
